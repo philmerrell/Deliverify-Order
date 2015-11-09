fe88d1f83dcf49dee29699665060549e59898c6f
philmerrell <philmerrell@boisestate.edu>
2015-11-09 15:28:46 -0700

    Added auth with Github

--- app/services/UserService.ts
+++ app/services/UserService.ts
@@ -1,15 +1,25 @@
 /**
  * Created by philmerrell on 11/4/15.
  */
-import {Injectable} from "angular2/angular2";
-import * as Rx from "rx";
-import {User} from "../models/User";
+import {Injectable, EventEmitter} from "angular2/angular2";
 
-Injectable()
+@Injectable()
 export class UserService {
-    currentUser: Rx.Subject<User> = new Rx.BehaviorSubject<User>(null);
+    _emitter: EventEmitter = new EventEmitter();
+    userEmitter: any;
+    currentUser: any;
 
-    public setCurrentUser(newUser: User): void {
-        this.currentUser.onNext(newUser);
+    constructor() {
+        this.currentUser = false;
+        this.userEmitter = this._emitter.toRx();
+    }
+
+    setCurrentUser(newUser): void {
+        this.currentUser = newUser;
+        this.userEmitter.next(newUser);
+    }
+
+    getCurrentUser() {
+        return this.currentUser;
     }
 }
