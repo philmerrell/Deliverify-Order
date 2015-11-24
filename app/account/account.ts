7ac40470851b933be4e5b03e262daebce4937690
philmerrell <philmerrell@boisestate.edu>
2015-11-24 09:19:02 -0700

    Clean up for user feedback

--- app/account/account.ts
+++ app/account/account.ts
@@ -1,7 +1,10 @@
-import {Page, NavController} from 'ionic/ionic';
+import {Page, NavController, Modal} from 'ionic/ionic';
 import {UserService} from "../services/UserService";
 import {AuthService} from "../services/AuthService";
 import {Checkout} from '../checkout/checkout';
+import {CartService} from '../data/cart';
+import {LocationModal} from "../location-modal/location-modal";
+import {PaymentModal} from "../payment-modal/payment-modal";
 
 
 @Page({
@@ -13,9 +16,11 @@ export class Account {
   authService: AuthService;
   currentUser: any;
 
-  constructor(authService: AuthService, userService: UserService, nav: NavController) {
+  constructor(authService: AuthService, cartService: CartService, userService: UserService, nav: NavController, modal: Modal) {
     this.userService = userService;
     this.authService = authService;
+    this.cartService = cartService;
+    this.modal = modal;
     this.nav = nav;
     this.currentUser = this.userService.getCurrentUser();
     this.userService.userEmitter.subscribe((user) => {
@@ -30,6 +35,14 @@ export class Account {
   checkout() {
     //this.modal.open(Checkout);
     this.nav.push(Checkout);
+  }
+  
+  openLocationModal() {
+        this.modal.open(LocationModal);
+  }
+
+  openPaymentModal() {
+      this.modal.open(PaymentModal);
   }
 
 }
